bef2496ea7d119e68b641533bad16b9e94c7e403
David <91611885+FEDavid@users.noreply.github.com>
2023-05-07 14:03:52 +0100

    Removed unneeded files

--- genTxt.py
+++
@@ -1,9 +0,0 @@
-import io
-
-def generate_script(filename):
-    print(filename)
-    output = filename + "script.txt"
-    with open(filename, "r") as f:
-        content = f.read()
-    with open(output, "w") as f:
-        f.write(content)
